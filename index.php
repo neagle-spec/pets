efad4ab1effb66dcbfcc9d31c029bfb4f4741f39
neagle-spec <neagleaaron@hotmail.com>
2022-04-21 23:52:12 -0400

    Updated echo statement

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the Base class
+$f3 = Base::instance();
+
+// define a default root
+$f3->route('GET /', function () {
+    echo '<h1>Pet Home</h1>';
+
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+    }
+);
+
+// run fat free
+$f3->run();
